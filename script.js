01dd57afabaf410364124aab48e9231ec2e9b493
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2021-03-14 23:10:42 -0400

    variables added

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // Assignment code here
+
+// Variable Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// Variable Declaration 
+
+var confirmNumericCharacter;
+var confirmSpecialCharacter;
+var confirmLowerCase;
+var confirmUpperCase;
+
+
+// Determine parameters of password 
+
+var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");   
+var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+     
 
 
 // Get references to the #generate element
@@ -13,5 +39,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
